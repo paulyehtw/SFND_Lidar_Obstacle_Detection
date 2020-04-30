9e823b367bb0f1b171d5645ac1a2c9d9cc257706
Paul Yeh <pyeh.tw@icloud.com>
2020-04-30 12:50:56 +0200

    refactoring

--- src/environment.cpp
+++ src/environment.cpp
@@ -54,9 +54,10 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer)
     ProcessPointClouds<pcl::PointXYZ> *point_processor = new ProcessPointClouds<pcl::PointXYZ>();
     std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud =
         point_processor->SegmentPlane(pointCloud, 100, 0.2);
-    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
-    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
+    // renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(1, 1, 1));
 
+    // Cluster obstacles from segmented cloud
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters = point_processor->Clustering(segmentCloud.first, 1.0F, 3, 30);
     std::vector<Color> clusterColors{Color(1, 0, 0), Color(1, 1, 0), Color(0, 0, 1)};
     size_t clusterId = 0;
