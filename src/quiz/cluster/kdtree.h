88dbc65c7a4a92c88652dcb7373f612ce53bc03e
Paul Yeh <pyeh.tw@icloud.com>
2020-04-30 17:55:32 +0200

    implement KD tree searching

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -71,10 +71,44 @@ struct KdTree
 		}
 	}
 
+	void checkNode(const std::vector<float> target,
+				   Node *node,
+				   const float distanceTol,
+				   std::vector<int> &ids)
+	{
+		if (node != NULL)
+		{
+			if ((abs(node->point[0] - target[0]) <= distanceTol) && (abs(node->point[1] - target[1]) <= distanceTol))
+			{
+				float distance = sqrt((node->point[0] - target[0]) * (node->point[0] - target[0]) +
+									  (node->point[1] - target[1]) * (node->point[1] - target[1]));
+				if (distance <= distanceTol)
+				{
+					ids.push_back(node->id);
+				}
+			}
+			if (target[binaryDepth] - distanceTol < node->point[binaryDepth])
+			{
+				binaryDepth++;
+				checkNode(target, node->left, distanceTol, ids);
+			}
+			if (target[binaryDepth] + distanceTol > node->point[binaryDepth])
+			{
+				binaryDepth++;
+				checkNode(target, node->right, distanceTol, ids);
+			}
+		}
+	}
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		if (root != NULL)
+		{
+			binaryDepth = 0;
+			checkNode(target, root, distanceTol, ids);
+		}
 		return ids;
 	}
 };
